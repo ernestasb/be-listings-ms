e752d5f2fac8376a750621cd367a06b07fe57a4b
Erne <110843039+ernestasb@users.noreply.github.com>
2023-08-24 21:47:05 +0300

    Test fix

--- test/controllers/health_controller_test.rb
+++ test/controllers/health_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class HealthControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "health ok" do
+    get health_url
+    assert_response :success
+  end
+
 end
--- test/controllers/listings_controller_test.rb
+++ test/controllers/listings_controller_test.rb
@@ -1,36 +1,46 @@
-require "test_helper"
+require 'test_helper'
 
 class ListingsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    # what does the line below do?
+    
     @listing = listings(:one)
   end
 
-  test "should get index" do
-    get listings_url, as: :json
+  test 'should get index' do
+    get api_v1_listings_url, as: :json
     assert_response :success
   end
 
-  test "should create listing" do
-    assert_difference("Listing.count") do
-      post listings_url, params: { listing: { description: @listing.description, price: @listing.price, title: @listing.title } }, as: :json
+  test 'should create listing' do
+    assert_difference('Listing.count') do
+      post api_v1_listings_url,
+           params: {
+             listing: {
+               description: @listing.description,
+               price: @listing.price,
+               title: @listing.title
+             }
+           }, as: :json
     end
 
     assert_response :created
   end
 
-  test "should show listing" do
-    get listing_url(@listing), as: :json
+  test 'should show listing' do
+    get api_v1_listings_url(@listing), as: :json
     assert_response :success
   end
 
-  test "should update listing" do
-    patch listing_url(@listing), params: { listing: { description: @listing.description, price: @listing.price, title: @listing.title } }, as: :json
+  test 'should update listing' do
+    patch api_v1_listings_url(@listing), params:
+    { listing: { description: @listing.description, price: @listing.price, title: @listing.title } }, as: :json
     assert_response :success
   end
 
-  test "should destroy listing" do
-    assert_difference("Listing.count", -1) do
-      delete listing_url(@listing), as: :json
+  test 'should destroy listing' do
+    assert_difference('Listing.count', -1) do
+      delete api_v1_listings_url(@listing), as: :json
     end
 
     assert_response :no_content
